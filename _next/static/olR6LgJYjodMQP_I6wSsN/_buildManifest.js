9e878c8323c6ab713a52ccc71c6c53eb641191a5
calbone <calbone@users.noreply.github.com>
2021-07-08 06:41:27 +0000

    deploy: 6836ba97d33b3072a9d5d0479ba8b43a05d04692

--- _next/static/olR6LgJYjodMQP_I6wSsN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ebcccb9fd17888bc04aa.css","static/chunks/pages/index-c163122e680e4849b0ab.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
