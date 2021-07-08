1100b8ca7885d408520229878bea074bd90cc9df
YusukeKumamoto <kumamoto_yusuke@lightcafe.co.jp>
2021-07-08 18:07:22 +0900

    console削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log("aaaa")
   return <Component {...pageProps} />
 }
 export default MyApp
