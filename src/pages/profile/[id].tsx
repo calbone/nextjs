e709e53645d0e2542fa9a553fb2dd0122b0b1726
YusukeKumamoto <kumamoto_yusuke@lightcafe.co.jp>
2021-07-13 17:55:22 +0900

    SWR追加

---
+++ src/pages/profile/[id].tsx
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+function Profile() {
+  const router = useRouter();
+  const fetcher = async (id: string): Promise<any> =>
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
+      (res) => res.json()
+    );
+  const { data, error } = useSWR([`${router.query.id}`, "posts"], fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return <div>hello {data.title}!</div>;
+}
+
+export default Profile;
